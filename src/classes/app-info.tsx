f884926c0f8e54e587b4a739b2e3cc4e76d30352
evans.nkole <evans.nkole@cidrz.org>
2026-05-18 14:19:48 +0200

    refine-host

--- src/classes/app-info.tsx
+++ src/classes/app-info.tsx
@@ -1,8 +1,8 @@
 const AppInfo = {
   appName: "CIDRZ Central Lab Test Costing",
   appCode: "CCL Costing",
-  apiUrl: "http://127.0.0.1:8000/api/ccl/",
-  onlineApiUrl: "http://127.0.0.1:8000/api/ccl/",
+  apiUrl: "http://si-apps.cidrz.org/api/ccl/",
+  onlineApiUrl: "http://si-apps.cidrz.org/api/ccl/",
   localApiUrl: "http://127.0.0.1:8700/ccl/",
   configApiUrl: "sacco-config/1",
   uploadUrl: "",
